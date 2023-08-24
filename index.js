43d20c723bdce807e161f4cf122fa6cdf94e6d9a
padamanga <111278901+padamanga@users.noreply.github.com>
2023-08-23 20:45:48 -0500

    The YelpCamp web App

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
-require('dotenv').config();
+
+if (process.env.NODE_ENV !== "production")
+{
+    require('dotenv').config();
+}
+
 const express = require('express');
 const mongoose = require('mongoose');
 const path = require('path');
@@ -11,15 +16,26 @@ const flash = require('connect-flash');
 const passport = require('passport');
 const helmet = require('helmet');
 const LocalStrategy = require('passport-local');
+const User = require('./models/user');
+
 const mongoSanitize = require('express-mongo-sanitize');
 const MongoDBStore = require("connect-mongo");
 
-const Book = require('./models/books');
-const port = process.env.PORT || 3000;
+
+const port = process.env.PORT || process.env.MY_PORT;
 
 
+const userRoutes = require('./routes/users');
+const campgroundRoutes = require('./routes/campgrounds');
+const reviewRoutes = require('./routes/reviews');
+
+const catchAsync = require('./utils/catchAsync');
+const ExpressError = require('./utils/ExpressError');
+
+
+//process.env.DB_URL ||
 const dbUrl = process.env.DB_URL || 'mongodb://127.0.0.1:27017/yelpCamp';
-mongoose.connect(dbUrl, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(dbUrl, {useNewUrlParser: true, useUnifiedTopology: true});
 
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "connection error:"));
@@ -28,56 +44,151 @@ db.once("open", () => {
 });
 
 const app = express();
+
 app.engine('ejs', ejsMate);
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({extended:true}));
 app.use(methodOverride('_method'));
 app.use(express.static(path.join(__dirname, 'public')));
-// app.use(express.json());
+app.use(express.json());
+app.use(mongoSanitize({
+    replaceWith: '_'
+}));
 
+const secret = process.env.CLOUDINARY_SECRET || 'thisshouldbeabettersecret!';
 
-
-app.get('/', (req, res) => {
-    res.send("hello");
+const store = MongoDBStore.create({
+    mongoUrl: dbUrl,
+    touchAfter: 24 * 60 * 60,
+    crypto: {
+        secret
+    }
 });
 
-app.get('/add-note', (async (req, res) => {
-    try {
-        await Book.insertMany([
-            {
-                title: "Sons of Anarchy",
-                body:"The body text is Here..."
-            },
-            {
-                title: "Kirikou Film",
-                body: "The boy of Africae..."
-            }
-        ])
-    } catch (error) {
-        console.log(`Oh No! Error: ${error}`);
+store.on("error", function (e) {
+    console.log("SESSION STORE ERROR", e)
+});
+
+const sessionConfig = {
+    store,
+    name: 'session',
+    secret,
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+        httpOnly: true,
+        // secure: true,
+        expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
+        maxAge: 1000 * 60 * 60 * 24 * 7
     }
+};
+
+app.use(session(sessionConfig));
+app.use(flash());
+app.use(cookieParser());
+app.use(helmet({contentSecurityPolicy: false}));
+
+
+// const scriptSrcUrls = [
+//     "https://stackpath.bootstrapcdn.com/",
+//     "https://api.tiles.mapbox.com/",
+//     "https://api.mapbox.com/",
+//     "https://kit.fontawesome.com/",
+//     "https://cdnjs.cloudflare.com/",
+//     "https://cdn.jsdelivr.net",
+// ];
+// const styleSrcUrls = [
+//     "https://kit-free.fontawesome.com/",
+//     "https://stackpath.bootstrapcdn.com/",
+//     "https://api.mapbox.com/",
+//     "https://api.tiles.mapbox.com/",
+//     "https://fonts.googleapis.com/",
+//     "https://use.fontawesome.com/",
+// ];
+// const connectSrcUrls = [
+//     "https://api.mapbox.com/",
+//     "https://a.tiles.mapbox.com/",
+//     "https://b.tiles.mapbox.com/",
+//     "https://events.mapbox.com/",
+// ];
+// const fontSrcUrls = [];
+// app.use(
+//     helmet.contentSecurityPolicy({
+//         directives: {
+//             defaultSrc: [],
+//             connectSrc: ["'self'", ...connectSrcUrls],
+//             scriptSrc: ["'unsafe-inline'", "'self'", ...scriptSrcUrls],
+//             styleSrc: ["'self'", "'unsafe-inline'", ...styleSrcUrls],
+//             workerSrc: ["'self'", "blob:"],
+//             objectSrc: [],
+//             imgSrc: [
+//                 "'self'",
+//                 "blob:",
+//                 "data:",
+//                 "https://res.cloudinary.com/YOURNAME/", //SHOULD MATCH YOUR CLOUDINARY ACCOUNT!
+//                 "https://images.unsplash.com/",
+//             ],
+//             fontSrc: ["'self'", ...fontSrcUrls],
+//         },
+//     })
+// );
+
+
+
+
+
+// Authentication
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());       // Store the user in the session
+passport.deserializeUser(User.deserializeUser());   // Un-store the user in the session
+
+
+app.use((req, res, next) => {
+    console.log(req.session);
+    res.locals.currentUser = req.user;
+    res.locals.success = req.flash('success');
+    res.locals.error = req.flash('error');
+    next();
+});
+
+
+
+app.use('/', userRoutes);
+app.use('/campgrounds', campgroundRoutes);
+app.use('/campgrounds/:id/reviews', reviewRoutes);
+
+
+
+app.get('/', catchAsync(async (req, res)=>{
+    await res.render('home');
 }));
 
 
+app.get('/homePage', (req, res)=>
+{
+    res.render('myHome');
+});
 
-app.get('/', (async(req, res) => {
-    const book = await Book.find();
-    if(book)
-    {
-        res.json(book);
-    }
-    else{
-        res.send('Somthing went wrong!');;
-    }
-    // res.render('home');
-}));
+// '*'--> for every path
+app.all('*', (req, res, next)=>
+{
+    next(new ExpressError('Page Not Found', 404));
+});
 
 
+app.use((err, req,res, next )=>
+{
+    const {statusCode = 500} = err;
+    if (!err.message) err.message = 'Oh No, Something went wrong'
+    res.status(statusCode).render('error', {err});
+});
 
 
-app.listen(port, () => {
+
+app.listen(port, ()=>{
     console.log(`Listening from PORT: ${port}`);
 });
-
